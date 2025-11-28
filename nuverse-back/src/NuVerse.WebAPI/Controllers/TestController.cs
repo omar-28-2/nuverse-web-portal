74142aeb8251b4352888cffe697931ca47e26bff
omar-28-2 <o.abdelrahman2228@nu.edu.eg>
2025-11-28 20:34:46 +0200

    Nuverse Backbone is ready!!

---
+++ nuverse-back/src/NuVerse.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NuVerse.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTest()
+        {
+            return Ok("NuVerse API is alive ✨");
+        }
+    }
+}
---
+++ nuverse-back/src/NuVerse.WebAPI/Program.cs
@@ -0,0 +1,32 @@
+using NuVerse.Application;
+using NuVerse.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
