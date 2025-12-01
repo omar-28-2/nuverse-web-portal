cbea9abd7622b7c9c1dd951c3fd5bad0d97dc77b
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-12-01 22:39:47 +0200

    Adding limit for submissting form to avoid milicious emails

--- nuverse-back/src/NuVerse.WebAPI/Program.cs
+++ nuverse-back/src/NuVerse.WebAPI/Program.cs
@@ -1,8 +1,34 @@
+using System.Threading.RateLimiting;
 using NuVerse.Application;
 using NuVerse.Infrastructure;
+using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+// Add rate limiting policies
+builder.Services.AddRateLimiter(options =>
+{
+    // Per-IP fixed window: 6 requests per minute for contact submissions
+    options.AddPolicy("ContactPolicy", context =>
+    {
+        var ip = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+        return RateLimitPartition.GetFixedWindowLimiter(ip, _ => new FixedWindowRateLimiterOptions
+        {
+            PermitLimit = 6,
+            Window = TimeSpan.FromMinutes(1),
+            QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+            QueueLimit = 0
+        });
+    });
+
+    options.OnRejected = async (context, token) =>
+    {
+        var logger = context.HttpContext.RequestServices.GetService(typeof(ILogger<Program>)) as ILogger;
+        logger?.LogWarning("Rate limit rejected request from {IP}", context.HttpContext.Connection.RemoteIpAddress);
+        context.HttpContext.Response.StatusCode = 429;
+        await context.HttpContext.Response.WriteAsync("Too Many Requests", token);
+    };
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddApplication();
@@ -21,6 +47,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enable rate limiting middleware
+app.UseRateLimiter();
 
 app.UseAuthorization();
 
